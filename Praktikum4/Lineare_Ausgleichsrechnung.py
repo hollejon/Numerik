501393812ad965e0d8339ce406760c5dee09d2ef
SpirouE <terasun@hotmail.de>
2024-03-12 12:29:52 +0100

    add given code

--- Praktikum4/Lineare_Ausgleichsrechnung.py
+++ Praktikum4/Lineare_Ausgleichsrechnung.py
@@ -1,1 +1,43 @@
 import numpy as np
+from scipy.linalg import solve_triangular
+import matplotlib.pyplot as plt
+from numpy.linalg import cholesky
+
+
+data = np.genfromtxt('data.txt')
+
+L = cholesky(A)
+
+
+class myFit:
+  def __init__(self, data):
+      self.setData(data)
+      self.c = None       # Klassen Variable für die Modell Koeffizienten
+      self.omega = omega
+
+  def setData(self, data):
+      self.ti = data[:,0] # Zeitstempel
+      self.ui = data[:,1] # Messwerte
+
+  def computeCoefs(self, n=5):
+      self.n = n
+
+    #   <<snipp>>
+
+    #   self.c = <<snipp>>
+
+  def compute(self,t):
+      if not type(mf.c) == np.ndarray:
+          self.computeCoefs()
+
+    #   y = <<snipp>>
+
+      return y
+  
+# Objekt instanzieren
+mf = myFit(data)
+# Koeffizienten berechnen
+mf.computeCoefs(5)
+# Visualisieren
+plt.plot(t,mf.compute(t))
+plt.show()
