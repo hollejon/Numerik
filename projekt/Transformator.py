7ea88d41d04d00b0c2add048a4c9893b278bb461
hollejon <126668919+hollejon@users.noreply.github.com>
2024-04-19 20:55:12 +0200

    p1

---
+++ projekt/Transformator.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+f =     10**5       # Frequenz in HZ
+R1 =    800         # Primärwiderstand in Ohm
+Lp =    50*10**-6   # Primärinduktivität in H    
+Lps =   150*10**-6  # Streuinduktivität in H
+Lsp =   150*10**-6  # Streuinduktivität in H
+R2 =    2           # Sekundärwiderstand in Ohm
+Ls =    500*10**-6  # Sekundärinduktivität in H
+
+
+
+# Anfangsbedingungen
+
+
+
+
+
+
